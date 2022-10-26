d6de1f101f1771f1612b0e933e813ade880ec113
GabrielDoddy94 <gabrieldoddy1994@gmail.com>
2022-10-26 13:53:27 -0300

    About Checkout page component

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,11 +1,31 @@
-import React from 'react'
-import styled from 'styled-components'
-import { PageHero } from '../components'
-import aboutImg from '../assets/hero-bcg.jpeg'
+import React from "react";
+import styled from "styled-components";
+import { PageHero } from "../components";
+import aboutImg from "../assets/hero-bcg.jpeg";
 
 const AboutPage = () => {
-  return <h4>about page</h4>
-}
+  return (
+    <main>
+      <PageHero title="about" />
+      <Wrapper className="page section section-center">
+        <img src={aboutImg} alt="nice desk" />
+
+        <article>
+          <div className="title">
+            <h2>our story</h2>
+            <div className="underline"></div>
+          </div>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laborum et
+            obcaecati exercitationem saepe debitis necessitatibus iure quis
+            laudantium vitae nihil sequi doloribus est molestiae, amet nesciunt
+            quas. Alias, error rem?
+          </p>
+        </article>
+      </Wrapper>
+    </main>
+  );
+};
 
 const Wrapper = styled.section`
   display: grid;
@@ -33,5 +53,5 @@ const Wrapper = styled.section`
   @media (min-width: 992px) {
     grid-template-columns: 1fr 1fr;
   }
-`
-export default AboutPage
+`;
+export default AboutPage;
