2e7b428e60711e56e0ea64eb8aa51603ecf6db94
GabrielDoddy94 <gabrieldoddy1994@gmail.com>
2022-10-27 17:13:06 -0300

    FeaturedProducts component

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,7 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const Loading = () => {
-  return <h4>loading element</h4>
-}
+  return (
+    <div className="section section-center">
+      <div className="laoding"></div>
+    </div>
+  );
+};
 
-export default Loading
+export default Loading;
