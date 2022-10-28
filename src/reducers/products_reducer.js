c2af76ee77994cc332bc10e80c5705b090c90635
GabrielDoddy94 <gabrieldoddy1994@gmail.com>
2022-10-28 15:04:01 -0300

    fetch single product

--- src/reducers/products_reducer.js
+++ src/reducers/products_reducer.js
@@ -16,6 +16,7 @@ const products_reducer = (state, action) => {
   if (action.type === SIDEBAR_CLOSE) {
     return { ...state, isSidebarOpen: false };
   }
+
   if (action.type === GET_PRODUCTS_BEGIN) {
     return { ...state, products_loading: true };
   }
@@ -33,6 +34,28 @@ const products_reducer = (state, action) => {
   }
   if (action.type === GET_PRODUCTS_ERROR) {
     return { ...state, products_loading: false, products_error: true };
+  }
+
+  if (action.type === GET_SINGLE_PRODUCT_BEGIN) {
+    return {
+      ...state,
+      single_product_loading: true,
+      single_product_error: false,
+    };
+  }
+  if (action.type === GET_SINGLE_PRODUCT_SUCCESS) {
+    return {
+      ...state,
+      single_product_loading: false,
+      single_product: action.payload,
+    };
+  }
+  if (action.type === GET_SINGLE_PRODUCT_ERROR) {
+    return {
+      ...state,
+      single_product_loading: false,
+      single_product_error: true,
+    };
   }
 
   throw new Error(`No Matching "${action.type}" - action type`);
