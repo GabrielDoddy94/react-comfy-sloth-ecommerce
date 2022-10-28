b987c61ba614e120e4ae87dbf3c7cb99c024239c
GabrielDoddy94 <gabrieldoddy1994@gmail.com>
2022-10-28 14:50:13 -0300

    format price functionality

--- src/components/Product.js
+++ src/components/Product.js
@@ -16,7 +16,7 @@ const Product = ({ image, name, price, id }) => {
 
       <footer>
         <h5>{name}</h5>
-        <p>${price}</p>
+        <p>{formatPrice(price)}</p>
       </footer>
     </Wrapper>
   );
