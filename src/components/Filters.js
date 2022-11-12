1e285a1209db73733cc5a889b004023734fcc6b3
GabrielDoddy94 <gabrieldoddy1994@gmail.com>
2022-11-12 14:41:42 -0300

    filters - price

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -116,6 +116,19 @@ const Filters = () => {
               })}
             </div>
           </div>
+
+          <div className="form-control">
+            <h5>price</h5>
+            <p className="price">{formatPrice(price)}</p>
+            <input
+              type="range"
+              name="price"
+              min={min_price}
+              max={max_price}
+              value={price}
+              onChange={updateFilters}
+            />
+          </div>
         </form>
       </div>
     </Wrapper>
