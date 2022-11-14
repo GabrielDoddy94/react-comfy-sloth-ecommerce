e50ddfbf1116d6fe334e91615630d74455b82adc
GabrielDoddy94 <gabrieldoddy1994@gmail.com>
2022-11-14 11:29:39 -0300

    cart page setup

--- src/context/cart_context.js
+++ src/context/cart_context.js
@@ -24,6 +24,12 @@ export const CartProvider = ({ children }) => {
     dispatch({ type: ADD_TO_CART, payload: { id, color, amount, product } });
   };
 
+  const removeItem = id => {};
+
+  const toggleAmount = (id, value) => {};
+
+  const clearCart = () => {};
+
   return (
     <CartContext.Provider value={{ ...state, addToCart }}>
       {children}
