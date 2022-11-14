5f37b48c058c0050fc7e7ff8ebba579e8d8a208d
GabrielDoddy94 <gabrieldoddy1994@gmail.com>
2022-11-14 12:09:05 -0300

    localStorage cart

--- src/context/cart_context.js
+++ src/context/cart_context.js
@@ -8,8 +8,20 @@ import {
   COUNT_CART_TOTALS,
 } from "../actions";
 
+const getLocalStorage = () => {
+  let cart = localStorage.getItem("cart");
+
+  console.log(cart);
+
+  if (cart) {
+    return JSON.parse(localStorage.getItem("cart"));
+  } else {
+    return [];
+  }
+};
+
 const initialState = {
-  cart: [],
+  cart: getLocalStorage(),
   total_items: 0,
   total_amount: 0,
   shipping_fee: 534,
@@ -29,6 +41,10 @@ export const CartProvider = ({ children }) => {
   const toggleAmount = (id, value) => {};
 
   const clearCart = () => {};
+
+  useEffect(() => {
+    localStorage.setItem("cart", JSON.stringify(state.cart));
+  }, [state.cart]);
 
   return (
     <CartContext.Provider value={{ ...state, addToCart }}>
