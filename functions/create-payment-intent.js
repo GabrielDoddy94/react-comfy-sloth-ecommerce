23282d571d424da3bc3956f02cdd97cf1e1914dc
GabrielDoddy94 <gabrieldoddy1994@gmail.com>
2022-11-17 13:30:50 -0300

    netlify function - create-payment-setup

---
+++ functions/create-payment-intent.js
@@ -0,0 +1,17 @@
+// domain/.netlify/functions/create-payment-intent
+
+exports.handler = async function (event, context) {
+  if (event.body) {
+    const { cart, shipping_fee, total_amount } = JSON.parse(event.body);
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(cart),
+    };
+  }
+
+  return {
+    statusCode: 200,
+    body: "Create Payment Intent",
+  };
+};
